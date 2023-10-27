b5dc2bb785082eda29c6b2dd6b06a127705140ba
IlyaGorelik <ilyha.gor@mail.ru>
2023-10-27 16:51:47 +0200

    admin_settings | auth and languages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        if (app()->isProduction()) {
+            return;
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(AdminSeeder::class);
     }
 }
